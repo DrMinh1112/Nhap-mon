7a9ec109338b71d1f9969e46da2ebe3ca3a505de
DrMinh1112 <drminh1112@gmail.com>
2025-01-03 10:11:48 +0700

    Update sketch_dec21a.ino

--- sketch_dec21a/sketch_dec21a.ino
+++ sketch_dec21a/sketch_dec21a.ino
@@ -1,38 +1,43 @@
 #include <LiquidCrystal_I2C.h>
 #include <Servo.h>
-LiquidCrystal_I2C lcd(___,16,2)
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 int angle1;
 int angle2;
 int angle3;
 
-typedef struct{
+typedef struct {
   char name[10];
-  int check; //bằng 0 nếu ko có xe đỗ và ngược lại
-}p_lot; //kieu du lieu gom cac ô đỗ xe
-p_lot p[3]={{"a1",0},{"a2",0},{"a3",0}};// mảng gồm các chỗ đỗ xe
+  int check;                                             //bằng 0 nếu ko có xe đỗ và ngược lại
+} p_lot;                                                 //kieu du lieu gom cac ô đỗ xe
+p_lot p[3] = { { "a1", 0 }, { "a2", 0 }, { "a3", 0 } };  // mảng gồm các chỗ đỗ xe
 
-class usonic_ss{
-  public:
-  Servo sv; // servo ứng với cảm biến đó
-  int id_p_lot[3];// mảng chứa chỉ số của mảng p[] tức là những ô mà cảm biến đấy sẽ detect
+class usonic_ss {
+public:
+  Servo sv;         // servo ứng với cảm biến đó
+  int id_p_lot[3];  // mảng chứa chỉ số của mảng p[] tức là những ô mà cảm biến đấy sẽ detect
   int echopin;
   int trigpin;
-  usonic_ss(): id_p_lot{0,1,2}, echopin(7), trigpin(8) {} // không truyền tham số thì mặc định là như này
-  usonic_ss(int id[],int trig, int echo): echopin(echo),trigpin(trig) {
-    for(int i=0;i<3;i++){
-      id_p_lot[i]=id[i];
+  usonic_ss()
+    : id_p_lot{ 0, 1, 2 }, echopin(7), trigpin(8) {}  // không truyền tham số thì mặc định là như này
+  usonic_ss(int id[], int trig, int echo)
+    : echopin(echo), trigpin(trig) {
+    for (int i = 0; i < 3; i++) {
+      id_p_lot[i] = id[i];
     }
   }
-  ~usonic_ss(){}
+  ~usonic_ss() {}
   void detect(int x);
   void rotate();
-  void attachServo(int pin) { sv.attach(pin); }
+  void attachServo(int pin) {
+    sv.attach(pin);
+  }
 };
-usonic_ss ss; //cam bien tên u1 (vì mình có một cảm biến nên khao báo 1 biến thôi nhiều thì sẽ là mảng)
+usonic_ss ss;  //cam bien tên u1 (vì mình có một cảm biến nên khao báo 1 biến thôi nhiều thì sẽ là mảng)
 
 void setup() {
-  ss.attachServo(9); //pin cua servo ung voi u1
+  Serial.begin(9600);
+  ss.attachServo(9);  //pin cua servo ung voi u1
   pinMode(ss.echopin, INPUT);
   pinMode(ss.trigpin, OUTPUT);
   lcd.init();
@@ -44,46 +49,42 @@ void loop() {
   displayonlcd(p);
 }
 
-void usonic_ss:: detect(int x){
-  digitalWrite(trigpin,LOW);//setup lại, không truyền điện cho chân trig
+void usonic_ss::detect(int x) {
+  digitalWrite(trigpin, LOW);  //setup lại, không truyền điện cho chân trig
   delayMicroseconds(2);
-  digitalWrite(trigpin, HIGH);//bắt đầu truyền điện cho chân trig
-  delayMicroseconds(10);//chân trig sẽ phát ra sóng siêu âm trong vòng 20 micro giây// NOTE: phát sóng bao lâu là tối ưu?
-  digitalWrite(trigpin, LOW);//Tắt chân echo
-  
-  long khoang_tg=pulseIn(echopin, HIGH,1000);//Hàm pulsein đo thời gian micro giây từ lúc chân echo nhận sóng phản hồi(HIGH) đến lúc kết thúc
-  int quang_dg=ceil(khoang_tg*0.034/2);// công thức tính quãng đường
+  digitalWrite(trigpin, HIGH);  //bắt đầu truyền điện cho chân trig
+  delayMicroseconds(10);        //chân trig sẽ phát ra sóng siêu âm trong vòng 20 micro giây// NOTE: phát sóng bao lâu là tối ưu?
+  digitalWrite(trigpin, LOW);   //Tắt chân echo
 
-  Serial.println(khoang_tg);
+  long khoang_tg = pulseIn(echopin, HIGH);     //Hàm pulsein đo thời gian micro giây từ lúc chân echo nhận sóng phản hồi(HIGH) đến lúc kết thúc
+  int quang_dg = ceil(khoang_tg * 0.034 / 2);  // công thức tính quãng đường
   Serial.println(quang_dg);
-  if (quang_dg <= 2){ 
-    p[x].check=2;
+  if (quang_dg <= 2) {
+    p[x].check = 2;
     Serial.println(" Da có vat chan cam bien, can di chuyen vat can");
-  }
-  else if(2<quang_dg&&quang_dg<10){
-    p[x].check=1;
+  } else if (2 < quang_dg && quang_dg < 10) {
+    p[x].check = 1;
     Serial.println(" Da co xe do o day");
-  }
-  else{
-    p[x].check=0;
+  } else {
+    p[x].check = 0;
     Serial.println("Bai do con trong");
   }
-}; 
+};
 
 //quay 3 góc và detect() sử dụng hàm detect ở trên
-void usonic_ss::rotate(){
-    // Quay servo đến góc 60 độ
-    sv.write(60);
-    detect(id_p_lot[0]);
-    delay(1000); // Đợi 1 giây để servo ổn định
-    // Quay servo đến góc 120 độ
-    sv.write(120);
-    detect(id_p_lot[1]);
-    delay(1000);
-    // Quay servo đến góc 180 độ
-    sv.write(180);
-    detect(id_p_lot[2]);    
-    delay(1000);
+void usonic_ss::rotate() {
+  // Quay servo đến góc 60 độ
+  sv.write(60);
+  detect(id_p_lot[0]);
+  delay(2000);  // Đợi 1 giây để servo ổn định
+  // Quay servo đến góc 120 độ
+  sv.write(120);
+  detect(id_p_lot[1]);
+  delay(2000);
+  // Quay servo đến góc 180 độ
+  sv.write(180);
+  detect(id_p_lot[2]);
+  delay(2000);
 };
 
 void displayonlcd(p_lot pl[]) {
@@ -91,20 +92,17 @@ void displayonlcd(p_lot pl[]) {
   for (int i = 0; i < 3; i++) {
     if (pl[i].check == 0) {
       lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print ("Hay do xe vao o: ");
-      lcd.setCursor(0,1);
-      lcd.print (pl[i].name);
+      lcd.setCursor(0, 0);
+      lcd.print("Hay do xe vao o: ");
+      lcd.setCursor(0, 1);
+      lcd.print(pl[i].name);
       con_slot = true;
       break;
     }
   }
   if (con_slot == false) {
     lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print ("Da het cho de xe");
-    lcd.setCursor(0,1);
-    lcd.print ("           ");
+    lcd.setCursor(0, 0);
+    lcd.print("Da het cho de xe");
   }
 }
-
